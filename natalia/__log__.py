fb891881cc15e1a2561e82d6ac46a62b33fe7be4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:47:30 -0300

    Created natalia/__log__.py at Timestamp: 2018-08-01 18:47:29

---
+++ natalia/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Thu Aug 02 2018 03:37:44.114 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    TARZAN['width'] = 100
+TypeError: 'str' object does not support item assignment
+'''},
