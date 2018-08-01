74393855d0e0935bd688ecefc3e55a9f7558aec8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:40:27 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-01 19:40:27

--- angie/__log__.py
+++ angie/__log__.py
@@ -185,3 +185,21 @@ NameError: name 'nome' is not defined
     w = wave.open("http://audiosoundclips.com/wp-content/uploads/2016/01/Dubtrain.mp3","r")
 AttributeError: 'module' object has no attribute 'open'
 '''},
+{'date': 'Wed Aug 01 2018 16:35:41.312 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 74
+    game()
+  module <module> line 67
+    mixer.init()
+AttributeError: 'module' object has no attribute 'init'
+'''},
