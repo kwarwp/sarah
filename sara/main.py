6902f1740936a86fdc8a88ccbc18a92ab2562a8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:02:34 -0300

    Created sara/main.py at Timestamp: 2018-08-01 18:02:34

---
+++ sara/main.py
@@ -0,0 +1,9 @@
+# sarah.sara.main.py
+"""A cinderela saiu para tomar um chá. Barbie iria encontrar com a Cinderela mas ainda estava em seu castelo.
+como estava muito tarde o homem aranha deu uma carona para as duas. todos tomaram um susto ao encontrar o yoda na floresta."""
+from _spy.vitollino.main import cena, Elemento, Texto, STYLE
+from _spy.vitollino.main import INVENTARIO as inv
+
+STYLE['width'] = 700
+
+
