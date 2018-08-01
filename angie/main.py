dc5e5c793832c76f774d69aed2082b57fce36c2b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:10:32 -0300

    Saved angie/main.py at Timestamp: 2018-08-01 18:10:32

--- angie/main.py
+++ angie/main.py
@@ -11,3 +11,8 @@ from _spy.vitollino.main import INVENTARIO  as  inv
 
 STYLE['width'] = 700
 
+YODA = "https://vignette.wikia.nocookie.net/disney/images/9/95/Master_Yoda.png/revision/latest/scale-to-width-down/258?cb=20161024220430&path-prefix=pt-br";
+HOMEM-ARANHA = "https://static.wixstatic.com/media/dd9c2d_63f04202d4a4430cb4d4c479ed917932~mv2.png/v1/fill/w_269,h_224,al_c,usm_0.66_1.00_0.01/dd9c2d_63f04202d4a4430cb4d4c479ed917932~mv2.png"
+BARBIE = "http://1.bp.blogspot.com/-grB1NKIXwqw/UzRDyJiVLeI/AAAAAAAAdSE/v6FWRgv-2Hc/s1600/Baibie-em-png-queroimagem-cei%C3%A7a-crispim+(2).png"
+
+PANTANO = "
