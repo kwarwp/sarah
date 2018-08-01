81c0fa4a9cdc6ed731e575e29d90dd8d33311079
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:51:26 -0300

    Saved callie/main.py at Timestamp: 2018-08-01 18:51:26

--- callie/main.py
+++ callie/main.py
@@ -7,7 +7,7 @@ STYLE['width'] = 700
 BARBIE = "http://4.bp.blogspot.com/-x3s2-mSb_pg/UcsTQuFUwuI/AAAAAAAANVs/5NQp3g72WQE/s1600/9.jpg"
 CINDERELA = "https://1.bp.blogspot.com/-V2tLZSbQiXk/WD8fBBsCwDI/AAAAAAAACAo/ik35d327hAAa6YfTwuN-tD2RozNXOgzpwCLcB/s1600/23.png"
 HOMEMARANHA = "https://2.bp.blogspot.com/-Sx288TzGsV0/V5E7WpC1S2I/AAAAAAAALNE/46iReRzHSMslkQKxOgLr6jb5Pl4LTXEnwCLcB/s1600/B78144.jpg"
-MESTRE YODA = "https://1.bp.blogspot.com/-R9novqwzprA/V1LnhAeWayI/AAAAAAAAEuQ/6jOkfjgu6KsauBmAMBGBuWZmRHpkoa8QACLcB/s1600/Yodar.png"
+YODA = "https://1.bp.blogspot.com/-R9novqwzprA/V1LnhAeWayI/AAAAAAAAEuQ/6jOkfjgu6KsauBmAMBGBuWZmRHpkoa8QACLcB/s1600/Yodar.png"
 TARZAN = "https://vignette.wikia.nocookie.net/vsdebating/images/6/68/Tarzan.png/revision/latest?cb=20170707201517"
 FLORESTA = "https://img.elo7.com.br/product/zoom/1360B4C/painel-floresta-g-frete-gratis-lona-festa-infantil.jpg"
 CASTELO = "https://vignette.wikia.nocookie.net/ouat/images/a/af/Castelo.png/revision/latest?cb=20121027032937&path-prefix=pt-br"
