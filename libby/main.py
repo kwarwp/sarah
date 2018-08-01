c99a21a1ee3d686e1b0516f00797d5a98d7b0340
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:26:13 -0300

    Saved libby/main.py at Timestamp: 2018-08-01 18:26:13

--- libby/main.py
+++ libby/main.py
@@ -6,3 +6,4 @@ CINDERELA = "https://imagensemoldes.com.br/wp-content/uploads/2018/04/Imagem-de-
 PANTANO = "https://get.pxhere.com/photo/tree-nature-forest-swamp-wilderness-branch-plant-sunlight-leaf-wildlife-stream-jungle-autumn-vegetation-rainforest-deciduous-wetland-woodland-habitat-tilos-ecosystem-palma-biome-old-growth-forest-natural-environment-geographical-feature-atmospheric-phenomenon-woody-plant-temperate-broadleaf-and-mixed-forest-temperate-coniferous-forest-1190614.jpg"
 BARBIE = "http://1.bp.blogspot.com/-LJWdHVZII9U/UdNuaOHnodI/AAAAAAAA5Mo/N0Pv10SU_Dk/s1599/1zbrssk.png"
 CASTELO = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Pa%C3%A7os_do_Concelho_de_Viana_do_Castelo.jpg/800px-Pa%C3%A7os_do_Concelho_de_Viana_do_Castelo.jpg"
+ALICE = "https://3.bp.blogspot.com/-o7Y78sYGkjY/V6O1G7WysSI/AAAAAAAAMO8/IG0Q7cJKKcYA70fLNINSaLG02t9fQT52QCLcB/s400/ALICE%2B%25283%2529.png"
