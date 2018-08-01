62919d082afcec677d12d6b82d52c1d45c8d66cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:35:32 -0300

    Saved roxanne/main.py at Timestamp: 2018-08-01 18:35:32

--- roxanne/main.py
+++ roxanne/main.py
@@ -6,11 +6,12 @@ STYLE['widht']=700
 
 BARBIE = "http://4.bp.blogspot.com/-XNMtYvCUcRc/UdNuuEBNjaI/AAAAAAAA5O8/GgshXcDS_vQ/s480/barbie1-758236+(1).png"
 TARZAN = "http://imagens.us/gifs/tarzan/Tarzan%20(22).png"
-HOMEM ARANHA = "http://3.bp.blogspot.com/-uv54jlZoqfU/T9eUlpul_CI/AAAAAAAAFos/NCmWuK9X9Nw/s1600/Homem-Aranha-png-Queroimagem.com+%284%29.png"
+HOMEM_ARANHA = "http://3.bp.blogspot.com/-uv54jlZoqfU/T9eUlpul_CI/AAAAAAAAFos/NCmWuK9X9Nw/s1600/Homem-Aranha-png-Queroimagem.com+%284%29.png"
 ALICE = "https://3.bp.blogspot.com/-o7Y78sYGkjY/V6O1G7WysSI/AAAAAAAAMO8/IG0Q7cJKKcYA70fLNINSaLG02t9fQT52QCLcB/s1600/ALICE%2B%25283%2529.png"
 FLORESTA = "https://img.elo7.com.br/product/zoom/12F2BAA/painel-de-festa-floresta-3-painel-em-tecido.jpg"
 CASTELO = "http://www.imagenspng.com.br/wp-content/uploads/2015/04/branca-de-neve-cute-castelo-03.png"
 CHA = "https://cintiasiloto.files.wordpress.com/2011/10/alice-e-gato-risonho.png"
 
 def game():
-    
+    castelo = Cena(img=CHA)
+    homem aranha = Cena(img=HOMEM_ARANHA)
