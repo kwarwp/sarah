7a610960aad728684b480bfd7f8b5f2f030802f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:09:08 -0300

    Created courtney/main.py at Timestamp: 2018-08-01 18:09:08

---
+++ courtney/main.py
@@ -0,0 +1,7 @@
+# sarah.courtney.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+from _spy.vitollino.main import INVENTARIO as inv
+
+STYLE['width'] = 700
+
+YODA = "https://upload.wikimedia.org/wikipedia/pt/6/6f/Yoda_Attack_of_the_Clones.png"
