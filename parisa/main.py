d1cc127c2e3042dbb629ce3e9a776bfbe953503a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:19:45 -0300

    Saved parisa/main.py at Timestamp: 2018-08-01 18:19:45

--- parisa/main.py
+++ parisa/main.py
@@ -1,2 +1,22 @@
 # sarah.parisa.main.py
+
+'''Mestre Yoda marcou um cha com alice, quando chegou, ela nao estava la e ele achou estranho
+mas resolveu nao disperdicar toda aquela comida. Depois de beber o priemiro gole do cha, mestre Yoda
+adormeceu e acordou em um pantano. Assim que acordou ouviu um grito: "Tarzan, nao surfe nesse lago de fogo!", 
+ele reconheceu a voz: era alice Mestre Yoda tentou encontralos, mas se viu envolto num emaranhado de teias e 
+no final do caminho viu um importante aliado nesta busca, seu amigo de sempre, homem aranha.
+'''
+
+
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+from _spy.vitollino.main import INVENTARIO as inv
+STYLE['width'] = 700
+
+HomemAranha = "http://3.bp.blogspot.com/-uv54jlZoqfU/T9eUlpul_CI/AAAAAAAAFos/NCmWuK9X9Nw/s1600/Homem-Aranha-png-Queroimagem.com+%284%29.png"
+Pantano = "http://4.bp.blogspot.com/-hE1u7v-c2v0/Vjq_YbcIwfI/AAAAAAAAACk/gPbNpntWPl4/s1600/P%25C3%25A2ntano%2BManchac%2B1.jpg"
+Tarzan
+Alice
+Yoda
+Fogo = "http://2.bp.blogspot.com/-HOxFQa9iGv8/T6s9kuBLAAI/AAAAAAAAGiw/sjK-EuhC5gE/s1600/fire.png"
+
+
