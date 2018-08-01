1674fdb8af25ebdff89c3881233de672fe12ee6c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:46:23 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-01 18:46:22

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -35,3 +35,21 @@ NameError: name 'casa' is not defined
     cha.direita=casa
 NameError: name 'casa' is not defined
 '''},
+{'date': 'Wed Aug 01 2018 16:39:49.130 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    game()
+  module <module> line 24
+    cha.direita = casa
+NameError: name 'casa' is not defined
+'''},
