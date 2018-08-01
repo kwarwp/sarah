319fd92af1743f525fcfcb2deb56dd0c432cde9e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 14:34:48 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-01 17:34:48

--- angie/__log__.py
+++ angie/__log__.py
@@ -29,3 +29,20 @@ SyntaxError: invalid syntax
     echo("hellow world");
 NameError: name 'echo' is not defined
 '''},
+{'date': 'Wed Aug 01 2018 14:30:02.58 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''hellow world
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    print("olá, " . nome$);
+AttributeError: 'str' object has no attribute 'nome$'
+'''},
