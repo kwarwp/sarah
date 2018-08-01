0b4e2f1c493b4a0353201aa8b7fabf7e847b98d5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:20:21 -0300

    Saved parisa/main.py at Timestamp: 2018-08-01 18:20:21

--- parisa/main.py
+++ parisa/main.py
@@ -15,7 +15,7 @@ STYLE['width'] = 700
 HomemAranha = "http://3.bp.blogspot.com/-uv54jlZoqfU/T9eUlpul_CI/AAAAAAAAFos/NCmWuK9X9Nw/s1600/Homem-Aranha-png-Queroimagem.com+%284%29.png"
 Pantano = "http://4.bp.blogspot.com/-hE1u7v-c2v0/Vjq_YbcIwfI/AAAAAAAAACk/gPbNpntWPl4/s1600/P%25C3%25A2ntano%2BManchac%2B1.jpg"
 Tarzan
-Alice
+Alice = "http1.bp.blogspot.com-sJykitngfbQVk9VANJv_4IAAAAAAAALaQUi9Mw02CRSUs1600ALICE_TEKA%2B24.png"
 Yoda
 Fogo = "http://2.bp.blogspot.com/-HOxFQa9iGv8/T6s9kuBLAAI/AAAAAAAAGiw/sjK-EuhC5gE/s1600/fire.png"
 
