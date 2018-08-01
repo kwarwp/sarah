c2e1790bb41a4ba0a3f76f629a0e4869bce9d514
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:53:22 -0300

    Saved natalia/main.py at Timestamp: 2018-08-01 18:53:22

--- natalia/main.py
+++ natalia/main.py
@@ -13,7 +13,7 @@ PANTANO = "https://vignette.wikia.nocookie.net/starwars/images/f/f0/InYouMustGo-
 
 def game():
     tarzan = Cena(img=TARZAN)
-    tarzan_casa = Elemento(img=TARZAN,style=dict(width=100, height=100))
+    tarzan_casa = Elemento(img=TARZAN,style=dict(right=100, width=200, height=200))
     casa = Cena(img=CASA)
     homemaranha = Cena(img=HOMEMARANHA)
     castelo = Cena(img=CASTELO)
