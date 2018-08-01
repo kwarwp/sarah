98fcb56662e408a774e6c516075b994083b5a2e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:27:45 -0300

    Saved courtney/main.py at Timestamp: 2018-08-01 18:27:45

--- courtney/main.py
+++ courtney/main.py
@@ -5,3 +5,8 @@ from _spy.vitollino.main import INVENTARIO as inv
 STYLE['width'] = 700
 
 YODA = "https://upload.wikimedia.org/wikipedia/pt/6/6f/Yoda_Attack_of_the_Clones.png"
+CASA_FLORESTA = "https://png.pngtree.com/element_origin_min_pic/16/07/17/17578b4cfbd4e93.jpg"
+CINDERELA = "https://png.pngtree.com/element_pic/00/16/09/0957d1be2423435.jpg" 
+CHA_DE_DESANIVERSARIO = "http://images1.fanpop.com/images/photos/1700000/Alice-in-Wonderland-1951-alice-in-wonderland-1758796-500-372.jpg"
+HOMEM_ARANHA = "https://cdn1.mundodastribos.com/wp-admin/uploads/2011/07/homemaranha11.jpg"
+CASTELO = "https://i0.wp.com/melprandatoemorlando.com.br/wp-content/uploads/2015/04/IMG_0311-copy.jpg"
