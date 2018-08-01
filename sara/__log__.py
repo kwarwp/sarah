fec5bd8d2d5fbbd0288985a5fbae43afb66fcdb8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:48:06 -0300

    Saved sara/__log__.py at Timestamp: 2018-08-01 18:48:06

--- sara/__log__.py
+++ sara/__log__.py
@@ -61,3 +61,34 @@ Traceback (most recent call last):
   module <module> line 4
     from _spy.vitollino.main import cena, Elemento, Texto, STYLE
 '''},
+{'date': 'Wed Aug 01 2018 15:48:41.265 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from _spy.vitollino.main import cena, Elemento, Texto, STYLE
+ImportError: cannot import name 'cena'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from _spy.vitollino.main import cena, Elemento, Texto, STYLE
+'''},
