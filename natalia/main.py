a080b2db7004da200369e6a63712d790242b939d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:32:33 -0300

    Saved natalia/main.py at Timestamp: 2018-08-01 18:32:32

--- natalia/main.py
+++ natalia/main.py
@@ -1,1 +1,14 @@
 # sarah.natalia.main.py
+from _sypy.vitollino.main import Cena, Elemento, Texto, STYLE
+from _sypy.vitollino.main import INVENTARIO as inv
+
+STYLE['width'] = 700
+
+TARZAN = ""
+CASA = ""
+HOMEMARANHA = ""
+CASTELO = ""
+CINDERELA = ""
+PANTANO = ""
+
+def game():
