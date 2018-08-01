9702f657ed5d2d8e3385db726906e37404458eab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:49:13 -0300

    Created stacy/__log__.py at Timestamp: 2018-08-01 18:49:13

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 01 2018 15:48:23.249 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  HOMEM ARANHA = "https://opcaonews.com.br/wp-content/uploads/2017/07/homem-aranha.png"
+         ^
+SyntaxError: invalid syntax
+'''},
