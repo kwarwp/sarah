948bb6421085bb4a9585db785a210c67f9412225
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:22:12 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-08-01 19:22:12

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -71,3 +71,21 @@ NameError: name 'chadois' is not defined
     homem_aranha.entra(chadois)
 NameError: name 'chadois' is not defined
 '''},
+{'date': 'Wed Aug 01 2018 16:16:57.733 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 47
+    game()
+  module <module> line 38
+    homem_aranha.entra(chadois)
+NameError: name 'chadois' is not defined
+'''},
