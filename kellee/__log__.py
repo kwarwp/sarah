a5fb31fe9103e5a37c5830dba7ff6a8a75ac8ac4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:01:50 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-01 19:01:50

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -67,3 +67,19 @@ SyntaxError: invalid syntax
                                                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 01 2018 16:55:16.373 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    yoda==Elemento(img=YODA, tit="Yoda", Stile=dict(left=250, top=250, width=100, heigth="200px"))
+NameError: name 'yoda' is not defined
+'''},
