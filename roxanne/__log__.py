f21a42a5df4a63053ebd2f2a98cd050bf5f1b8b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:42:44 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-08-01 19:42:43

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -89,3 +89,34 @@ NameError: name 'chadois' is not defined
     homem_aranha.entra(chadois)
 NameError: name 'chadois' is not defined
 '''},
+{'date': 'Wed Aug 01 2018 16:43:22.123 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from _spy.vitollino.main import Music
+ImportError: cannot import name 'Music'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 264
+    action()
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 4
+    from _spy.vitollino.main import Music
+'''},
