e3d30abd5d07b45f174446b7de926f3efc7c9019
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:22:27 -0300

    Saved kathryn/main.py at Timestamp: 2018-08-01 18:22:26

--- kathryn/main.py
+++ kathryn/main.py
@@ -5,4 +5,8 @@ from _spy.vitollino.main import INVENTARIO as inv
 STYLE['width'] = 700
 
 BARBIE = "http://pngimg.com/uploads/barbie/barbie_PNG34.png"
-
+HOMEM_ARANHA = "https://2.bp.blogspot.com/-Sx288TzGsV0/V5E7WpC1S2I/AAAAAAAALNE/46iReRzHSMslkQKxOgLr6jb5Pl4LTXEnwCLcB/s1600/B78144.jpg"
+ALICE = "https://3.bp.blogspot.com/-o7Y78sYGkjY/V6O1G7WysSI/AAAAAAAAMO8/IG0Q7cJKKcYA70fLNINSaLG02t9fQT52QCLcB/s1600/ALICE%2B%25283%2529.png"
+FLORESTA = "https://vignette.wikia.nocookie.net/producaocultural/images/9/96/Floresta_Sombria.png/revision/latest?cb=20110605015107&path-prefix=pt-br"
+CASTELO = "https://img.elo7.com.br/product/zoom/14C140D/painel-banner-paisagem-castelo-3-00x2-00-paisagem-castelo.jpg"
+CIDADE = "https://png.pngtree.com/element_origin_min_pic/16/12/28/72a8682347bade87f11b895fb8a2b2d6.jpg" 
