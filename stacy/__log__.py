e4caa99b0b05dcf2c891ce65266517177c18b75a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:57:49 -0300

    Saved stacy/__log__.py at Timestamp: 2018-08-01 18:57:49

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -6,3 +6,21 @@
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 01 2018 15:57:00.602 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 42
+    jogo()
+  module <module> line 35
+    mestreyoda = Elemento(img=MESTREYODA, tit="mestreyoda", style=dict(left=300, 
+NameError: name 'MESTREYODA' is not defined
+'''},
