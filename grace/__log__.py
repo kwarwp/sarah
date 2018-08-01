064378526190a3e8a105ebc0eb1ef189c29de854
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:54:04 -0300

    Created grace/__log__.py at Timestamp: 2018-08-01 18:54:04

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 01 2018 15:45:26.220 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 27
+  tarzan = Elemento(img=TARZAN, tit="Tarzan", style=dict(left=100, top=100, width=100, rigth=100)
+                                                                                                       ^
+SyntaxError: invalid syntax
+'''},
