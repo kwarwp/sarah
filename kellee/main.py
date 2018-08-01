211bc5a0dd185941dce466071e15cf305bc33793
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:45:16 -0300

    Saved kellee/main.py at Timestamp: 2018-08-01 18:45:16

--- kellee/main.py
+++ kellee/main.py
@@ -1,26 +1,29 @@
 # sarah.kellee.main.py
 
-'''Yoda estava na cidade, Depois foi encontrar a Alice para tomarem um cha, Ao fim, voltou pra casa para assistir Tarzan.'''
-
+'''Yoda estava na cidade;
+Depois foi encontrar a Alice para tomarem um cha;
+Ao fim, voltou pra casa para assisitir Tarzan;
+'''
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
-from _spy.vitollino.main import INVENTARIO as inv
+from _spy.vitollino.main import INVENTARIO as inv 
 
-STYLE['width'] = 700
+STYLE['with'] = 700 
 
-YODA = "https://2.bp.blogspot.com/-V8V5RPZUD1M/V1LnlY7xeOI/AAAAAAAAEu4/lKAEq2C0z5Y1NUmrGvSdspKHiz6RiM9gACLcB/s640/Yodauur.png"
-ALICE = "https://3.bp.blogspot.com/-o7Y78sYGkjY/V6O1G7WysSI/AAAAAAAAMO8/IG0Q7cJKKcYA70fLNINSaLG02t9fQT52QCLcB/s1600/ALICE%2B%25283%2529.png"
-TARZAN = "https://vignette.wikia.nocookie.net/disney-infinity/images/e/e0/Young_Tarzan.png"
-CIDADE = "https://s2.glbimg.com/1Wh9VrSZokI1CoRhHpdgeTapEnI=/e.glbimg.com/og/ed/f/original/2018/02/22/cidade.jpg"
-CHA = "https://conteudo.imguol.com.br/c/noticias/b4/2017/09/29/cha-com-alice-minipanquecas-americanas-1506696952132_1920x1080.jpg"
-CASA = "https://www.plantapronta.com.br/projetos/140/01.jpg"
+YODA = "https://3.bp.blogspot.com/-mnbOb6pCOVk/V0nMMYhny-I/AAAAAAAAhJE/RyTXasp8OykQIbNMJhA4R9lVlcjiuHliACLcB/s1600/star-wars-yoda-sixth-scale-hot-toys-silo-902738.png"
+TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png"
+ALICE = "https://2.bp.blogspot.com/-gbvgbBcZsIg/V2YHCoD6yMI/AAAAAAAAGOk/RevcsxQgQ7YhqyL5j8gDx-zrRq8MriNzACLcB/s320/1b.png"
+CHA = "http://weshareideas.com.br/blog/wp-content/uploads/2016/08/cha-da-alice-1.jpg"
+CASA = "https://images.homify.com/c_fill,f_auto,q_auto:eco,w_440/v1439854761/p/photo/image/538636/faz.m.alegre_ft.Marcelo_Marona_5562.jpg"
+CIDADE = "https://i2.wp.com/cbncuritiba.com/wp-content/uploads/2017/05/por-do-sol-na-cidade-com-arranha-ceus_1127-192.jpg"
 
 def game():
     cidade = Cena(img=CIDADE)
     cha = Cena(img=CHA)
-    casa = Cena(img=CASA)
-    cidade.direita = cha
-    cha.direita = casa
-    casa.direita = cidade
+    Casa = Cena(img=CASA)
+
+    cidade.direita=cha
+    cha.direita=casa
+    casa.direita=cidade
     
     cidade.vai()
     
