33879eee87354f654b13f2b4e9e6671c621d06b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:27:52 -0300

    Saved angie/main.py at Timestamp: 2018-08-01 18:27:51

--- angie/main.py
+++ angie/main.py
@@ -3,7 +3,7 @@
 # Yoda está no pantano meditando quando teve uma visão onde,
 # Homem-aranha está no rolé na cidade e recebeu um convite.
 # Passou na casa da Barbie e, juntos
-# Foram ao baile funk no castelo.
+# Foram ao baile funk no Castelo das Pedras.
 
 
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
