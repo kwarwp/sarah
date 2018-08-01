8661af4649da9a2a2454fcdeedda45310e779352
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:21:20 -0300

    Created parisa/__log__.py at Timestamp: 2018-08-01 18:21:20

---
+++ parisa/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Wed Aug 01 2018 15:14:09.443 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    Tarzan
+NameError: name 'Tarzan' is not defined
+'''},
