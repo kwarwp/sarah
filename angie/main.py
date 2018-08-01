06b7456b0f6e0de9333a0d32f498f78ac7b8eef8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 14:51:35 -0300

    Created angie/main.py at Timestamp: 2018-08-01 17:51:35

---
+++ angie/main.py
@@ -0,0 +1,6 @@
+# sarah.angie.main.py
+
+# Yoda está no pantano meditando quando teve uma visão onde,
+# Homem-aranha está no rolé na cidade e recebeu um convite.
+# Passou na casa da Barbie e, juntos
+# Foram ao baile funk no castelo.
