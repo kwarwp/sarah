b08f175e2dcc831dc34fdcaf9cd6516bd9cf86c7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:35:52 -0300

    Saved courtney/__log__.py at Timestamp: 2018-08-01 19:35:51

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -138,3 +138,10 @@ SyntaxError: invalid syntax
                                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 01 2018 16:29:59.694 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  yoda = Elemento(img=YODA,
+                                                                                               ^
+SyntaxError: invalid syntax
+'''},
