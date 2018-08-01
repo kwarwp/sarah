7681b87f9775ee96955ed86d254cdc0ad198d723
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:34:33 -0300

    Saved natalia/main.py at Timestamp: 2018-08-01 18:34:33

--- natalia/main.py
+++ natalia/main.py
@@ -4,9 +4,9 @@ from _sypy.vitollino.main import INVENTARIO as inv
 
 STYLE['width'] = 700
 
-TARZAN = ""
-CASA = ""
-HOMEMARANHA = ""
+TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest/scale-to-width-down/399?cb=20170117061234"
+CASA = "http://pngimg.com/uploads/house/house_PNG50.png"
+HOMEMARANHA = "https://www.desicomments.com/wp-content/uploads/2017/02/Spiderman-Nice-Pic-600x432.png"
 CASTELO = ""
 CINDERELA = ""
 PANTANO = ""
