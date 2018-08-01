4a47c49dc260df0053b83184cfd9a9a8c582f48b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 14:49:42 -0300

    Created callie/__log__.py at Timestamp: 2018-08-01 17:49:42

---
+++ callie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Aug 02 2018 02:43:05.899 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  Barbie passeava na floresta
+          ^
+SyntaxError: invalid syntax
+'''},
