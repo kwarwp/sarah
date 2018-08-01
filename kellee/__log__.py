2c3cade1cce0fcaf520b5d5de3df83447202d59d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:13:46 -0300

    Saved kellee/__log__.py at Timestamp: 2018-08-01 19:13:46

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -83,3 +83,21 @@ SyntaxError: invalid syntax
     yoda==Elemento(img=YODA, tit="Yoda", Stile=dict(left=250, top=250, width=100, heigth="200px"))
 NameError: name 'yoda' is not defined
 '''},
+{'date': 'Wed Aug 01 2018 17:07:12.30 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    game()
+  module <module> line 37
+    alice = Elemento(img=alice, tit="Yoda", style=dict(left=250, top=250, width=200, heigth="200px"))
+UnboundLocalError: local variable 'alice' referenced before assignment
+'''},
