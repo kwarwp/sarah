9b85c4a9c9335618c63d38156ddb71897893591d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:26:24 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-01 19:26:24

--- angie/__log__.py
+++ angie/__log__.py
@@ -149,3 +149,21 @@ NameError: name 'nome' is not defined
     falaaranha = Texto(cidade, "Ih, caraca! É nóis! Vou levar " + nome + "!")
 NameError: name 'nome' is not defined
 '''},
+{'date': 'Wed Aug 01 2018 16:18:42.894 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 68
+    game()
+  module <module> line 51
+    falaaranha = Texto(cidade, "Ih, caraca! É nóis! Vou levar " + nome)
+NameError: name 'nome' is not defined
+'''},
