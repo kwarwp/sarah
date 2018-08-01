9c6fd704f7cecf5be4b6db98bcf2128054281b8c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:25:02 -0300

    Saved libby/main.py at Timestamp: 2018-08-01 18:25:02

--- libby/main.py
+++ libby/main.py
@@ -5,3 +5,4 @@ STYLE ['width'] = 700
 CINDERELA = "https://imagensemoldes.com.br/wp-content/uploads/2018/04/Imagem-de-Personagens-Cinderela-3-PNG-211x300.png"
 PANTANO = "https://get.pxhere.com/photo/tree-nature-forest-swamp-wilderness-branch-plant-sunlight-leaf-wildlife-stream-jungle-autumn-vegetation-rainforest-deciduous-wetland-woodland-habitat-tilos-ecosystem-palma-biome-old-growth-forest-natural-environment-geographical-feature-atmospheric-phenomenon-woody-plant-temperate-broadleaf-and-mixed-forest-temperate-coniferous-forest-1190614.jpg"
 BARBIE = "http://1.bp.blogspot.com/-LJWdHVZII9U/UdNuaOHnodI/AAAAAAAA5Mo/N0Pv10SU_Dk/s1599/1zbrssk.png"
+CASTELO = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Pa%C3%A7os_do_Concelho_de_Viana_do_Castelo.jpg/800px-Pa%C3%A7os_do_Concelho_de_Viana_do_Castelo.jpg"
