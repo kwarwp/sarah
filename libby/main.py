86ab9c8782f1529ea7583b800d0e5075e389d9b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:20:44 -0300

    Saved libby/main.py at Timestamp: 2018-08-01 18:20:44

--- libby/main.py
+++ libby/main.py
@@ -4,4 +4,4 @@ from _spy.vitollino.main import INVENTARIO as inv
 STYLE ['width'] = 700
 CINDERELA = "https://imagensemoldes.com.br/wp-content/uploads/2018/04/Imagem-de-Personagens-Cinderela-3-PNG-211x300.png"
 PANTANO = "https://get.pxhere.com/photo/tree-nature-forest-swamp-wilderness-branch-plant-sunlight-leaf-wildlife-stream-jungle-autumn-vegetation-rainforest-deciduous-wetland-woodland-habitat-tilos-ecosystem-palma-biome-old-growth-forest-natural-environment-geographical-feature-atmospheric-phenomenon-woody-plant-temperate-broadleaf-and-mixed-forest-temperate-coniferous-forest-1190614.jpg"
-BARBIE ="http://1.bp.blogspot.com/-LJWdHVZII9U/UdNuaOHnodI/AAAAAAAA5Mo/N0Pv10SU_Dk/s1599/1zbrssk.png"
+BARBIE = "http://1.bp.blogspot.com/-LJWdHVZII9U/UdNuaOHnodI/AAAAAAAA5Mo/N0Pv10SU_Dk/s1599/1zbrssk.png"
