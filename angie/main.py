f03d328758b75db0c8a2f321d78d4f9ed8ca491b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:20:11 -0300

    Saved angie/main.py at Timestamp: 2018-08-01 19:20:11

--- angie/main.py
+++ angie/main.py
@@ -55,7 +55,7 @@ def game():
     barbie.vai = falabarbie.vai    
     
     
-    galera = Elemento(img=GALERA, tit= "Galera do baile",style=dict(left=250, top=100, width=300, height="100px"))
+    galera = Elemento(img=GALERA, tit= "Galera do baile",style=dict(left=250, top=50, width=300, height="100px"))
     galera.entra(castelo)
     
     
