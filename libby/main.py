64b0155679b5f6486ca3b8fb3261c007f23147ea
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:08:55 -0300

    Created libby/main.py at Timestamp: 2018-08-01 18:08:55

---
+++ libby/main.py
@@ -0,0 +1,5 @@
+# sarah.libby.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+from _spy.vitollino.main import INVENTARIO as inv
+STYLE ['width'] = 700
+CINDERELA = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi4183-uMzcAhVLj5AKHZRyCVAQjRx6BAgBEAU&url=https%3A%2F%2Fimagensemoldes.com.br%2Fimagem-de-personagens-cinderela-3-png%2F&psig=AOvVaw2WrSNwQBy7C-NDrrZWb-x-&ust=1533233183796935"
