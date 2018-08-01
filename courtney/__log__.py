3901459639b79636ba474bd4e9f744c4c2d62799
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:58:58 -0300

    Saved courtney/__log__.py at Timestamp: 2018-08-01 18:58:58

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -71,3 +71,21 @@ NameError: name 'cinderela' is not defined
     cinderela.entra(casa_floresta)
 NameError: name 'cinderela' is not defined
 '''},
+{'date': 'Wed Aug 01 2018 15:53:05.566 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 29
+    game()
+  module <module> line 24
+    cinderela.entra(casa_floresta)
+NameError: name 'cinderela' is not defined
+'''},
