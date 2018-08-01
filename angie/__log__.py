291910ce97150b48a3f9247b49305d46cf2cb6d4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 14:31:03 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-01 17:31:02

--- angie/__log__.py
+++ angie/__log__.py
@@ -6,3 +6,10 @@
         ^
 SyntaxError: missing parenthesis in call to 'print'
 '''},
+{'date': 'Wed Aug 01 2018 14:26:17.470 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  echo "hellow world";
+        ^
+SyntaxError: invalid syntax
+'''},
