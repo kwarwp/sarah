7fb9d37384805dad29c6e030b46276d9da0b6074
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:15:53 -0300

    Saved libby/main.py at Timestamp: 2018-08-01 18:15:52

--- libby/main.py
+++ libby/main.py
@@ -3,4 +3,4 @@ from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 from _spy.vitollino.main import INVENTARIO as inv
 STYLE ['width'] = 700
 CINDERELA = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi4183-uMzcAhVLj5AKHZRyCVAQjRx6BAgBEAU&url=https%3A%2F%2Fimagensemoldes.com.br%2Fimagem-de-personagens-cinderela-3-png%2F&psig=AOvVaw2WrSNwQBy7C-NDrrZWb-x-&ust=1533233183796935"
-PANTANO = "https://www.google.com.br/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHjNz5uMzcAhXJHZAKHezPCQgQjRx6BAgBEAU&url=https%3A%2F%2Fpcdobalagoas.org.br%2F2018%2F03%2F07%2Feduardo-bomfim-pantano%2F&psig=AOvVaw0_et20CB3FCT2sYC4gdE-X&ust=1533233183172327"
+PANTANO = "https://get.pxhere.com/photo/tree-nature-forest-swamp-wilderness-branch-plant-sunlight-leaf-wildlife-stream-jungle-autumn-vegetation-rainforest-deciduous-wetland-woodland-habitat-tilos-ecosystem-palma-biome-old-growth-forest-natural-environment-geographical-feature-atmospheric-phenomenon-woody-plant-temperate-broadleaf-and-mixed-forest-temperate-coniferous-forest-1190614.jpg"
