7a878239771c6ceb703c2bd9296d37c87b72bc9e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:57:15 -0300

    Saved libby/main.py at Timestamp: 2018-08-01 18:57:15

--- libby/main.py
+++ libby/main.py
@@ -18,6 +18,10 @@ def games():
     floresta.direita = castelo
     castelo.esquerda = pantano
     
+    cinderela = Elemento(img= CINDERELA, tit="cinderela".style=dict(left=300,
+    top=100, width=50, height="200px"))
+    cinderela.entra(pantano)
+    
     pantano.vai()
     
 
