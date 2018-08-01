5b7868930db8d4d06f30ab11fc05b6d9a168eaf3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:23:32 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-01 19:23:32

--- angie/__log__.py
+++ angie/__log__.py
@@ -95,3 +95,21 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Aug 01 2018 16:18:46.597 GMt-0300 (Horário Padrão da Argentina) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 67
+    game()
+  module <module> line 44
+    NOME = imput("Qual é o seu nome?")
+NameError: name 'imput' is not defined
+'''},
