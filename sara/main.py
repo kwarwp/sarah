05b400afc19b970dde6e7171a6c6e5e651023cb5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:27:46 -0300

    Saved sara/main.py at Timestamp: 2018-08-01 18:27:46

--- sara/main.py
+++ sara/main.py
@@ -8,4 +8,8 @@ STYLE['width'] = 700
 
 Cinderela = "http://imagensemoldes.com.br/wp-content/uploads/2018/04/Imagem-de-Personagens-Cinderela-29-PNG-300x283.png"
 Barbie = "http://pngimg.com/uploads/barbie/barbie_PNG42.png"
-Home
+Home = "http://3.bp.blogspot.com/-uv54jlZoqfU/T9eUlpul_CI/AAAAAAAAFos/NCmWuK9X9Nw/s1600/Homem-Aranha-png-Queroimagem.com+%284%29.png"
+Yoda = "http://pngimg.com/uploads/starwars/starwars_PNG40.png"
+Cha da Alice = "http://iphotochannel.com.br/wp-content/uploads/2016/07/iphoto-como-fazer-um-cenario-barato-pra-fotografia-2.jpg"
+Luar = "https://st3.depositphotos.com/2739581/16638/v/600/depositphotos_166383456-stock-video-scary-autumn-forest-at-moonlight.jpg"
+Castelo = "https://vignette.wikia.nocookie.net/reinodesolaria/images/1/15/Castelo_real_branco.jpg/revision/latest?cb=20120906132605&path-prefix=pt-br"
