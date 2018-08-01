2374c2aee5a6efc7a63309ccb6535fa050339116
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:10:23 -0300

    Saved grace/main.py at Timestamp: 2018-08-01 19:10:23

--- grace/main.py
+++ grace/main.py
@@ -2,7 +2,8 @@
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE 
 from _spy.vitollino.main import INVENTARIO as inv
 
-STYLE['width'] = 700
+STYLE['width'] = 900
+
 
 TARZAN = "https://vignette.wikia.nocookie.net/disney/images/e/e2/Tarzan_Character.png/revision/latest?cb=20180126232838"
 ALICE = "https://3.bp.blogspot.com/-o7Y78sYGkjY/V6O1G7WysSI/AAAAAAAAMO8/IG0Q7cJKKcYA70fLNINSaLG02t9fQT52QCLcB/s1600/ALICE%2B%25283%2529.png"
