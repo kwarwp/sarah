c9de1a0ba4241b1b42a56f65531467197740304b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 15:40:32 -0300

    Saved courtney/main.py at Timestamp: 2018-08-01 18:40:32

--- courtney/main.py
+++ courtney/main.py
@@ -1,17 +1,12 @@
 # sarah.courtney.main.py
-from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+from _spy.vitollino.main import Cena, Elemento, texto, STYLE
 from _spy.vitollino.main import INVENTARIO as inv
 
-STYLE['width'] = 700
+STYLE['width']= 700
 
-YODA = "https://upload.wikimedia.org/wikipedia/pt/6/6f/Yoda_Attack_of_the_Clones.png"
-CASA_FLORESTA = "https://png.pngtree.com/element_origin_min_pic/16/07/17/17578b4cfbd4e93.jpg"
-CINDERELA = "https://png.pngtree.com/element_pic/00/16/09/0957d1be2423435.jpg" 
-CHA_DE_DESANIVERSARIO = "http://images1.fanpop.com/images/photos/1700000/Alice-in-Wonderland-1951-alice-in-wonderland-1758796-500-372.jpg"
-HOMEM_ARANHA = "https://cdn1.mundodastribos.com/wp-admin/uploads/2011/07/homemaranha11.jpg"
-CASTELO = "https://i0.wp.com/melprandatoemorlando.com.br/wp-content/uploads/2015/04/IMG_0311-copy.jpg"
+BARBIE = "http://geraldobarros.me/wp-content/uploads/2018/04/novo-de-festa-infantil-barbie-decora-o-barbie-paris-le-belly-atelie-festas-elo7.jpg"
+MESTRE_YODA = " http://www.coachevanroth.com/wp-content/uploads/2015/02/Yoda.jpg"
+CASTELO = "http://www.coachevanroth.com/wp-content/uploads/2015/02/Yoda.jpg"
 
 def game():
-    casa_floresta = Cena(img=CASA_FLORESTA)
-    cha_de_desaniversario = Cena(img=CHA_DE_DESANIVERSARIO)
-    castelo = Cena(img=CAASTELO)
+     castelo = Cena(img=CASTELO)
