a9e47da8cfbbb410d4f2d4eb5579366af464746f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-01 16:25:21 -0300

    Saved natalia/main.py at Timestamp: 2018-08-01 19:25:20

--- natalia/main.py
+++ natalia/main.py
@@ -7,8 +7,11 @@ STYLE['width'] = 700
 TARZAN = "https://vignette.wikia.nocookie.net/vsbattles/images/6/68/Tarzan.png/revision/latest/scale-to-width-down/399?cb=20170117061234"
 CASA = "http://pngimg.com/uploads/house/house_PNG50.png"
 HOMEMARANHA = "http://i.imgur.com/DuuGIgb.png"
+HOMEMARANHA_SUSTO = "https://vignette.wikia.nocookie.net/spiderman/images/9/93/Spidey_4.png/revision/latest?cb=20140816174042"
 CASTELO = "https://gallery.yopriceville.com/var/resizes/Free-Clipart-Pictures/Castles-PNG/Pink_Castle_PNG_Clipart_Image.png?m=1507172106"
+CASTELO_INTERIOR = "http://www.willpearson.co.uk/wp-content/images/studio-blog/windsor_castle_001.jpg"
 CINDERELA = "https://2.bp.blogspot.com/--O7PEMf5dgI/V1WAINEMFgI/AAAAAAAAGQg/MLO8j1u-iJEhPyuXEEVZ8hksqAfPTAYNQCLcB/s1600/cinderella-transp.png"
+CINDERELA_SUSTO = "https://www.disneyclips.com/imagesnewb3/images/clipcindyshoe.gif"
 PANTANO = "https://vignette.wikia.nocookie.net/starwars/images/f/f0/InYouMustGo-TCGCS.png/revision/latest?cb=20141218063223"
 
 def game():
@@ -17,18 +20,23 @@ def game():
     casa = Cena(img=CASA)
     homemaranha = Cena(img=HOMEMARANHA)
     homemaranha_castelo = Elemento(img=HOMEMARANHA,style=dict(top=192, left=315, width="90px", height="90px"))
+    homemaranha_susto_castelo = Elemento(img=HOMEMARANHA_SUSTO,style=dict(top=192, left=315, width="90px", height="90px"))
     castelo = Cena(img=CASTELO)
+    castelo_interior = Cena(img=CASTELO_INTERIOR)
     cinderela = Cena(img=CINDERELA)
+    cinderela_susto_castelo = Elemento(img=CINDERELA_SUSTO,style=dict(top=192, left=315, width="90px", height="90px"))
     pantano = Cena(img=PANTANO)
     
     tarzan.direita = casa
     tarzan_casa.entra(casa)
     casa.direita = homemaranha
     homemaranha.direita = castelo
-    homemaranha_castelo.entra(castelo)    
+    homemaranha_castelo.entra(castelo)
     castelo.direita = cinderela
-    cinderela.direita = castelo
-    castelo.direita = cinderela
+    cinderela.direita = castelo_interior
+    homemaranha_susto_castelo.entra(castelo_interior)
+    cinderela_susto_castelo.entra(castelo_interior)
+    castelo_interior.direita = cinderela
     cinderela.direita = pantano
     pantano.direita = tarzan
     
