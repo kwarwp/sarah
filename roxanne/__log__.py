0b2f6c644241701346e2bfe65b0e3e8810ec951d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 19:35:26 -0300

    Saved roxanne/__log__.py @2019-12-02 22:35

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -120,3 +120,19 @@ Traceback (most recent call last):
   module <module> line 4
     from _spy.vitollino.main import Music
 '''},
+{'date': 'Mon Dec 02 2019 21:35:23.462 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    from _spy.vitollino.main import Cena, Elemento, INVENTARIO, STYLE
+ImportError: _spy
+'''},
