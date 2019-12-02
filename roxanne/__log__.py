8f061c36b291f5cc7ca07fa3cd344a3bff6aafbb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 19:46:21 -0300

    Saved roxanne/__log__.py @2019-12-02 22:46

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -168,3 +168,21 @@ ImportError: _spy
     from _spy.vitollino.main import Cena, Elemento, INVENTARIO, STYLE
 ImportError: _spy
 '''},
+{'date': 'Mon Dec 02 2019 21:46:18.843 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 90
+    Elevador()
+  module <module> line 46
+    self.doggie = Elemento(Doggie, x=350, y=80, cena=predio, vai=self.entra_sai)
+NameError: name 'Doggie' is not defined
+'''},
