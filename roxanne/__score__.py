de6e474a1338ecf3ea164e2205bdf088021d0645
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-03 15:47:01 -0300

    Saved roxanne/__score__.py @2019-12-03 18:47

--- roxanne/__score__.py
+++ roxanne/__score__.py
@@ -1,2 +1,3 @@
 {'doc_id': 'f0d08389', 'carta': 'nada', 'casa': 'elevador', 'move': 'desce', 'ponto': 0, 'valor': 0, '_level': 0, 'tempo': 1575398816822},
   {'doc_id': 'f0d08389', 'carta': 'doggie', 'casa': 'doggie', 'move': 'entra', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1575398817864},
+  {'doc_id': 'f0d08389', 'carta': 'nada', 'casa': 'doggie', 'move': 'sai', 'ponto': 0, 'valor': 0, '_level': 1, 'tempo': 1575398820869},
