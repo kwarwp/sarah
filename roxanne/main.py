b0601f7bf70932dc0dc55b369c7d3a50afd486ba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 13:54:16 -0300

    Saved roxanne/main.py @2019-12-05 16:54

--- roxanne/main.py
+++ roxanne/main.py
@@ -36,7 +36,7 @@ class gameInicio:
     def __init__(self):
         gameInicio = Cena(CENAINICIO)
         gameInicio.vai()
-        dark= Elemento("",style=dict(width="1345px",height="600px",backgroundColor="black",opacity=0.7),cena=gameInicio)
+        dark = Elemento("",style=dict(width="1345px",height="600px",backgroundColor="black",opacity=0.7),cena=gameInicio)
         self.logotipo = Elemento(LOGO, x=370, y=30,w=650,h=400, cena=gameInicio)
         self.play = Elemento(PLAY, x=570, y=470,w=180,h=120, cena=gameInicio, vai = self.mostradia)
         
@@ -50,13 +50,13 @@ class gameInicio:
     
     def toca(self, ev=0):
         self.musica.sound.play()
-        self.musA.x= -1200
-        self.musB.x= 1200
+        self.musA.x = -1200
+        self.musB.x = 1200
     
     def pause(self, ev=0):
         self.musica.sound.pause()
-        self.musA.x= 1200
-        self.musB.x= -1200
+        self.musA.x = 1200
+        self.musB.x = -1200
     
     def elevador(self, ev=0):
         todos = Cena(FUNDODIA)
