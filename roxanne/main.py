3cdc2bdb50cc09c54d641d4657725a624db3d367
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-05 13:49:34 -0300

    Saved roxanne/main.py @2019-12-05 16:49

--- roxanne/main.py
+++ roxanne/main.py
@@ -34,14 +34,12 @@ STYLE ["height"] = "600px"
 #INICIO DO JOGO
 class gameInicio:
     def __init__(self):
-        
         gameInicio = Cena(CENAINICIO)
         gameInicio.vai()
         dark= Elemento("",style=dict(width="1345px",height="600px",backgroundColor="black",opacity=0.7),cena=gameInicio)
         self.logotipo = Elemento(LOGO, x=370, y=30,w=650,h=400, cena=gameInicio)
         self.play = Elemento(PLAY, x=570, y=470,w=180,h=120, cena=gameInicio, vai = self.mostradia)
         
-    
     def mostradia(self,ev=0):
         fake = Cena()
         fake.vai = self.elevador
